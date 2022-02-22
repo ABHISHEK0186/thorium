666cc2fd5f62bf6d76d7807c55fa9c413c94779b
ABHISHEK0186 <vinayak.abhishek11@gmail.com>
2022-02-22 11:50:53 +0530

    lodash

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 let obj= require('../logger/logger')
 let cal= require('../util/helper')
 let letter= require('../validator/formatter')
-let lod= require('../loadash/load')
+let lodash= require('lodash')
 
 router.get('/test-me', function (req, res) {
     obj.wel()
@@ -17,9 +17,33 @@ router.get('/test-me', function (req, res) {
 });
 
 router.get('/hello', function (req, res) {
-    lod.ch()
-    lod.ta()
-    lod.un()
+    let array1= ["jan","feb","mar","apr","may","jun","jul","aug","sept","oct","nov","dec"]
+    let result= lodash.chunk(array1,3);
+    console.log(result);
+
+
+
+    let arr= [1,3,5,7,9,11,13,15,17,19]
+    let res1= lodash.tail(arr);
+    console.log(res1);
+
+
+
+    let arrays= [1,2]
+    let arr2= [2,3]
+    let arr3= [4,5]
+    let arr4= [5,6]
+    let arr5 =[6,7,8]
+    let resu= lodash.union([arrays,arr2,arr3,arr4,arr5]);
+    console.log(resu);
+    
+
+    let movie1= ["horror" ,"The Shining"]
+    let movie2= ["drama" , "Titanic"]
+    let movie3= ["thriller" ,"shutter island"]
+    let movie4= ["fantasy", "Pans labyrinth"]
+    let outcome= lodash.fromPairs([movie1,movie2,movie3,movie4]);
+    console.log(outcome);
     res.send('My first ever api! yayy')
 });
 
