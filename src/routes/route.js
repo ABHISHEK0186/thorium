361eb3e350f0b4a311661bdbb2d9c8e4dee91f91
ABHISHEK0186 <vinayak.abhishek11@gmail.com>
2022-03-08 18:19:19 +0530

    assignment complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const userController= require("../controllers/userController")
+const userController= require("../controllers/userController");
+const { tokenCheck } = require('../midlleware/auth');
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+
 
 router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId", tokenCheck, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId", tokenCheck, userController.updateUser)
+
+router.delete("/users/:userId", tokenCheck, userController.deleteData)
 
 module.exports = router;
