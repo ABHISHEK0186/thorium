8a960ac479c4ae2a78433674b062fff44255baf8
ABHISHEK0186 <vinayak.abhishek11@gmail.com>
2022-03-09 17:13:25 +0530

    authorisation middleware

--- src/midlleware/auth.js
+++ src/midlleware/auth.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 let tokenCheck= function (req,res,next){
 let token = req.headers["x-Auth-token"];
 if (!token) token = req.headers["x-auth-token"];
@@ -7,8 +9,9 @@ else {next()}
 
 let authorisationCheck= function (req,res,next){
     let token= req.headers["x-auth-token"];
+    let decodedToken= jwt.verify(token, "functionup-thorium")
     let toBeUpdatedUserId= req.params.userId;
-    let loggedInUserId= token.userId;
+    let loggedInUserId= decodedToken.userId;
     if(loggedInUserId != toBeUpdatedUserId) return res.send({status:false, msg:"you are not authorized to perform this task"})
     else {next()}
 }
