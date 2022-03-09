93c2856de7933a3065acefb18741217eb43d0ca8
ABHISHEK0186 <vinayak.abhishek11@gmail.com>
2022-03-09 15:57:47 +0530

    authorisation mw added

--- src/midlleware/auth.js
+++ src/midlleware/auth.js
@@ -5,5 +5,14 @@ if (!token) return res.send({ status: false, msg: "token must be present" });
 else {next()}
 }
 
+let authorisationCheck= function (req,res,next){
+    let token= req.headers["x-auth-token"];
+    let toBeUpdatedUserId= req.params.userId;
+    let loggedInUserId= token.userId;
+    if(loggedInUserId != toBeUpdatedUserId) return res.send({status:false, msg:"you are not authorized to perform this task"})
+    else {next()}
+}
+
 
 module.exports.tokenCheck= tokenCheck
+module.exports.authorisationCheck= authorisationCheck
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController");
-const { tokenCheck } = require('../midlleware/auth');
+const { tokenCheck, authorisationCheck } = require('../midlleware/auth');
+
 
 
 
@@ -10,10 +11,10 @@ router.post("/users", userController.createUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", tokenCheck, userController.getUserData)
+router.get("/users/:userId", tokenCheck, authorisationCheck, userController.getUserData)
 
-router.put("/users/:userId", tokenCheck, userController.updateUser)
+router.put("/users/:userId", tokenCheck, authorisationCheck, userController.updateUser)
 
-router.delete("/users/:userId", tokenCheck, userController.deleteData)
+router.delete("/users/:userId", tokenCheck, authorisationCheck, userController.deleteData)
 
 module.exports = router;
